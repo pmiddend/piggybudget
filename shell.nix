2e72ed087e5dd796c675cb2ecb377d9d7d9f9e51
Philipp Middendorf <middendorf@plapadoo.de>
2018-05-24 13:51:58 +0200

    initial JS version

---
+++ shell.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import <nixpkgs> {} }:
+ 
+let fhs = pkgs.buildFHSUserEnv {
+  name = "android-env";
+  targetPkgs = pkgs: with pkgs;
+    [ git
+      gitRepo
+      gnupg
+      python2
+      curl
+      procps
+      openssl
+      gnumake
+      nettools
+      androidenv.platformTools
+      jdk
+      schedtool
+      utillinux
+      m4
+      gperf
+      perl
+      libxml2
+      zip
+      unzip
+      bison
+      nodePackages.react-native-cli
+      nodejs
+      yarn
+      flex
+      lzop
+    ];
+  multiPkgs = pkgs: with pkgs;
+    [ zlib
+    ];
+  runScript = "bash";
+  profile = ''
+    export USE_CCACHE=1
+    export ANDROID_JAVA_HOME=${pkgs.jdk.home}
+    export ANDROID_HOME=/home/philipp/Android/Sdk
+    export JAVA_HOME=${pkgs.jdk}
+  '';
+};
+in pkgs.stdenv.mkDerivation {
+  name = "android-env-shell";
+  nativeBuildInputs = [ fhs ];
+  shellHook = "exec android-env";
+}
